535251eae02bc7f6eaa14319923f5a46e97f8ebb
ÖMER KISAKOL <okisakol@gmail.com>
2024-09-14 11:01:51 +0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,44 @@
 import {  useEffect, useState } from 'react'
 import './index.css'
-import TextInput from "./components/TextInput.jsx";
 import ResponseLabel from "./components/ResponseLabel.jsx";
 
 function App() {
     const [termsAndConditionContent, setTermsAndConditionContent] = useState('');
-    const [inputPrompt, setInputPrompt] = useState('');
+    const [linkText, setLinkText] = useState('');
+    const [termsLink, setTermsLink] = useState('')
+    const [required, setRequired] = useState()
 
-
-    const handleInputChange = (e) => {
-        setInputPrompt(e.target.value);
-    };
     const handleTermsAndConditionChange = (e) => {
         setTermsAndConditionContent(e.target.value);
     };
 
-    const sendPrompt = async () => {
-        try {
-            const formData = new FormData();
-            formData.append('prompt', inputPrompt);
-
-            const response = await fetch('https://rc-akdemir.jotform.dev/intern-api/API/widgets/terms_and_conditions/ask_ai', {
-                method: 'POST',
-                body: formData,
-            });
-            const data = await response.json();
-            setTermsAndConditionContent(data.content);
-
-        } catch (error) {
-            console.error('API request failed:', error);
-        }
-    };
-
-
     useEffect(() => {
 
-    window.JFCustomWidget.subscribe("ready", () => {
-        const queryStringData = JFCustomWidget.getAllQueryString();
-        const initialTermsAndConditionContent  = queryStringData['termsContent'];
-        setTermsAndConditionContent(initialTermsAndConditionContent);
-
-        const initialInputPrompt  = queryStringData['AIPrompt'];
-        setInputPrompt(initialInputPrompt);
-
-        // console.log(JFCustomWidget.getWidgetData());
-        // console.log(queryStringData);
-    });
-
-    window.JFCustomWidget.subscribe("submit", function() {
-        window.JFCustomWidget.sendSubmit({
-            valid: true,
-            value: termsAndConditionContent
-        });
-    });
-        // console.log(termsAndConditionContent);
         // console.log(JFCustomWidget.getAllQueryString())
-    },[termsAndConditionContent]);
+        // console.log(JFCustomWidget.getWidgetData());
 
+        window.JFCustomWidget.subscribe("ready", () => {
+            const queryStringData = JFCustomWidget.getAllQueryString();
+            const initialTermsAndConditionContent  = queryStringData['termsContent'];
+            setTermsAndConditionContent(initialTermsAndConditionContent);
+            setLinkText(queryStringData['linkText']);
+            setTermsLink(queryStringData['termsLink']);
+            setRequired(JFCustomWidget.getWidgetData()['required'])
+
+        });
+
+    },[termsAndConditionContent]);
 
   return (
     <>
         <div>
-            <TextInput inputValue={inputPrompt} handleInputChange={handleInputChange} sendPrompt={sendPrompt} />
-            <ResponseLabel termsAndConditionContent={termsAndConditionContent} handleTermsAndConditionChange={handleTermsAndConditionChange}/>
+            <ResponseLabel
+                termsAndConditionContent={termsAndConditionContent}
+                handleTermsAndConditionChange={handleTermsAndConditionChange}
+                linkText={linkText}
+                termsLink={termsLink}
+                required={required}
+            />
         </div>
     </>
   )
--- src/components/ResponseLabel.jsx
+++ src/components/ResponseLabel.jsx
@@ -1,17 +1,131 @@
-import React from 'react';
+import React, {useEffect , useState} from "react";
 
-function ResponseLabel({ termsAndConditionContent }) {
+function ResponseLabel({ termsAndConditionContent, linkText, termsLink , required }) {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const clickableLink = (termsLink) => {
+        if (termsLink) {
+            return termsLink.startsWith("http://") || termsLink.startsWith("https://")
+                ? termsLink
+                : `http://${termsLink}`;
+        }
+    };
+
+    function boldTextInStars(text) {
+        return text.replace(/\*(.*?)\*/g, "<b>$1</b>");
+    }
+
+
+    useEffect(() => {
+        window.JFCustomWidget.subscribe("submit", function() {
+            if (termsAndConditionContent && typeof termsAndConditionContent === 'string' && termsAndConditionContent.trim().length !== 0){
+                if (isChecked) {
+                    window.JFCustomWidget.sendSubmit({
+                        valid: true,
+                        value: termsAndConditionContent
+                    });
+                } else {
+                    window.JFCustomWidget.sendSubmit({
+                        valid: false,
+                        value: 'None'
+                    });
+                }
+            } else {
+                if (isChecked) {
+                    console.log("else true")
+                    window.JFCustomWidget.sendSubmit({
+                        valid: true,
+                        value: 'None'
+                    });
+                } else {
+                    window.JFCustomWidget.sendSubmit({
+                        valid: false,
+                        value: 'None'
+                    });
+                }
+            }
+        });
+    }, [isChecked]);
+
+
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+
     return (
-        <div>
-            <label className="flex gap-1 text-xl">
-                <input type="checkbox" name="checkbox" id="checkbox"/>
-                <div className='font-medium'>
-                  <span>
-                       {termsAndConditionContent}
-                  </span>
-                </div>
-            </label>
-        </div>
+        <>
+            <div>
+                <label className="flex items-start p-2 gap-2 text-base">
+                    <input
+                        type="checkbox"
+                        name="checkbox"
+                        id="checkbox"
+                        onChange={handleCheckboxChange}
+                        checked={isChecked}
+                        className="mt-1 peer shrink-0 w-4 h-4 cursor-pointer rounded"
+                    />
+                    <div className="flex flex-col ">
+                        <div className="min-h-10 max-h-48 pr-4 leading-6 overflow-auto text-base">
+                            <div>
+                                <ol>
+                                    {termsAndConditionContent ? (
+                                        <ul>
+                                            {termsAndConditionContent
+                                                .split(/\n(?=\d+\.\s)/)
+                                                .map((item, index, arr) => {
+                                                    const formattedItem = boldTextInStars(item.trim());
+                                                    const match = formattedItem.match(/^(\d+\.\s)(.*)/s);
+                                                    if (!match) {
+                                                        return (
+                                                            <li key={index} style={{listStyleType: "none"}}>
+                                                                <span
+                                                                    dangerouslySetInnerHTML={{__html: formattedItem}}/>
+                                                                {index === arr.length - 1 && required && (
+                                                                    <span className="text-red-600">*</span>
+                                                                )}
+                                                            </li>
+                                                        );
+                                                    }
+                                                    const [_, number, rest] = match;
+                                                    return (
+                                                        <li key={index} style={{ listStyleType: "none" }}>
+                                                            <strong>{number}</strong> <span dangerouslySetInnerHTML={{ __html: rest.trim() }} />
+                                                            {index === arr.length - 1 && required && (
+                                                                <span className="text-red-600">*</span>
+                                                            )}
+                                                        </li>
+                                                    );
+                                                })}
+                                        </ul>
+                                    ) : (
+                                        <div className="text-base">
+                                            By clicking the submit button, I agree to terms and conditions.{" "}
+                                            {required ? (
+                                            <span className="text-red-600">*</span>
+                                            ):(<></>)}
+                                        </div>
+                                    )}
+                                </ol>
+                            </div>
+                        </div>{" "}
+                        <div>
+                            <span>
+                                <a
+                                    className="text-blue-500 font-bold underline"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    href={clickableLink(termsLink)}
+                                >
+                                    {linkText}
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </label>
+            </div>
+
+        </>
     );
 }
 
